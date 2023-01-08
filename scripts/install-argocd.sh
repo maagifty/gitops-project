2b15e2f138e0b1bbe30f102ec260e9512a9f2ee5
loveth oballe <oballe.a.loveth@gmail.com>
2023-01-07 23:33:10 -0800

    install argocd

---
+++ scripts/install-argocd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Add the Argo CD repository
+helm repo add argo https://argoproj.github.io/argo-cd
+
+# Update the repository cache
+helm repo update
+
+# Install Argo CD
+helm install argocd argo/argocd --create-namespace \
+  --set server.ingress.tls.enabled=true \
+  --set server.ingress.tls.secretName=argocd-server-tls \
+  --set server.ingress.hosts[0].host=argocd.example.com \
+  --set server.ingress.hosts[0].paths[0]=/ \
+  --set server.ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
+  --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/ssl-redirect"=true \
+  --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/force-ssl-redirect"=true \
+  --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/backend-protocol"=HTTPS
